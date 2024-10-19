de57c4df91a72c35a8e26bfaff530b140017ad99
Moazzem <tiyonbhuiyan.201@gmail.com>
2024-10-19 22:19:11 +0600

    update

--- src/Home/Hero/Hero.jsx
+++ src/Home/Hero/Hero.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const Hero = () => {
-    return (
-        <div>
 
-            I am hero section 
+
+    return (
+
+        <div>
+<div
+  className="hero h-[600px]"
+  style={{
+    backgroundImage: "url(https://i.ibb.co.com/WyQyxsL/gymh1.jpg)",
+  }}>
+  <div className="hero-overlay bg-opacity-40"></div>
+  <div className="hero-content">
+
+    <h1>Fitness <br />Center  </h1>
+    
+  </div>
+</div>
+
+          
             
         </div>
     );
